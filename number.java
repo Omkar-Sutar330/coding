c2bc40a8085e8bc1c6016da78d2806ca16e5e490
Omkar-Sutar330 <sutaromkar330@gmail.com>
2025-01-24 12:33:53 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,23 @@
+package ab;
+
+public class ae 
+{
+	public static void main(String[]args) {
+		//TODO Auto-generated method stub
+		int a=500,b=50,c=190;
+		
+		if(a>b)
+		{
+			if(a>c)
+				System.out.println ("a is larger");
+			else 
+				System.out.println("c is larger");
+		}
+		else if(b>c)
+		{
+			System.out.println("b is larger");
+		}
+		else 
+			System.out.println("c is larger");
+	}
+}
